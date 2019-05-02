abb823216cb8c1ec8b7b720ab487549b786c37b7
Andy Antes <aantes@tableau.com>
2019-05-02 15:54:54 -0700

    isAvailableFailsWhenRequestingTimeBeforeFivePM green

--- test/BabysitterPayTest.java
+++ test/BabysitterPayTest.java
@@ -1,2 +1,15 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.Date;
+
 public class BabysitterPayTest {
+    @Test
+    public void isAvailableFailsWhenRequestingTimeBeforeFivePM() {
+        BabysitterService service = new BabysitterService();
+        assertFalse(service.isAvailable(new GregorianCalendar(
+                2019, Calendar.MAY, 2, 2, 0).getTime()));
+    }
 }
